873c72db66825f152af70256a4c0e046fca835f3
Rob Denton <robdentonrg@gmail.com>
2014-09-15 15:39:26 -0700

    bunches of goodness

--- header.php
+++ header.php
@@ -5,16 +5,18 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=yes">
 
-	<title><?php echo get_the_title(); ?></title>
+	<title><?=the_title()?></title>
 	
 	<!-- [BEGIN] Zeppelin -->
 	<link rel="stylesheet" href="http://zeppelin.registerguard.com/prod/0.1.0/20140911/1/styles/zeppelin.min.css">
 	<script src="http://zeppelin.registerguard.com/prod/0.1.0/20140911/1/scripts/zeppelin.min.js"></script>
 	<!-- [END] Zeppelin -->	
 	
-	<!-- <link rel="stylesheet" href="style.css"> -->
+	<link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/base-min.css">
+	<link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/forms-min.css">
+	<link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/style.css">
 	
-	 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<?=wp_head()?> 
 	
 </head>
 <body>
@@ -24,15 +26,3 @@
 		<h6><a href="http://registerguard.com">The Register-Guard</a></h6>
 	</header>
 	<!-- [END] head -->
-	
-	<!-- [BEGIN] The loop -->
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		<h1><?php the_title(); ?></h1>
-	
-		<?php the_content(); ?>
-	
-	<?php endwhile; endif; ?>
-	<!-- [END] The loop -->
-	
-</body>
