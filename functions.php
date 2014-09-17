06d6af2e990f92d6eaff85f9effd88271e3edd66
Rob Denton <robdentonrg@gmail.com>
2014-09-16 17:54:24 -0700

    comment out function .pure-form

--- functions.php
+++ functions.php
@@ -5,9 +5,11 @@
 // See: http://purecss.io/forms/
 // Note: Have no idea what the 10 and 2 are
 //
+/*
 add_filter("gform_form_tag", "form_tag", 10, 2);
 function form_tag($form_tag, $form){
 	$form_tag = str_replace("form", "form class='pure-form'", $form_tag);
 	return $form_tag;
 }
+*/
 ?>
